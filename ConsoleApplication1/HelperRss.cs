34ca3767c5e6b03e0e86c6417d055dbd46fcae67
nreal <nreal@makingsense.com>
2016-07-29 17:02:55 -0300

    Rss validator

---
+++ ConsoleApplication1/HelperRss.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace ConsoleApplication1
+
+{
+    public static class HelperRss
+    {
+
+        public static bool RssUrlIsValid(string rssUrl)
+        {
+            try
+            {
+                SyndicationFeed rss = GetRss(rssUrl);
+                if (rss != null)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static SyndicationFeed GetRss(string rssUrl)
+        {
+            HttpWebRequest http = (HttpWebRequest)HttpWebRequest.Create(rssUrl);
+            http.Accept = "text/xml, */*";
+            http.Headers.Add(HttpRequestHeader.AcceptLanguage, "en-us");
+            http.UserAgent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; .NET CLR 3.5.30729;)";
+            http.KeepAlive = true;
+            http.AutomaticDecompression = DecompressionMethods.Deflate |
+                                         DecompressionMethods.GZip;
+            var response = http.GetResponse();
+            XmlReader reader = XmlReader.Create(response.GetResponseStream());
+
+            return SyndicationFeed.Load(reader);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"C:\Temp\WriteLines2.txt");
+
+                Console.WriteLine("Devuelve true si es un rss valido ");
+
+                foreach (var item in lines)
+                {
+                    var test = HelperRss.RssUrlIsValid(item);
+                    Console.WriteLine("\t" + test + "\t" + item);
+                }
+
+                Console.WriteLine("Press any key to exit.");
+                System.Console.ReadKey();
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("el archivo C:/Temp/WriteLines2.txt no existe");
+                System.Console.ReadKey();
+            }
+
+          
+        }
+    }
+}
